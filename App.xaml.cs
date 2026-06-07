a2df23a38a830ead62bf664304a54e1f6d2e6ddc
Helton-Carlos <heltoncarlospa>
2026-06-07 17:08:06 -0300

    chore: add project

---
+++ App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace e_commerceJoias;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace e_commerceJoias;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
